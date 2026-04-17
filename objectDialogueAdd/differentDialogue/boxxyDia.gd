0831f4f11211d960132f8883eae2f1d3c245468d
st00pid-cloud <justinemaesecret@gmail.com>
2026-04-17 20:59:23 +0800

    deleted dialogue function, populating environment

--- objectDialogueAdd/differentDialogue/boxxyDia.gd
+++
@@ -1,8 +0,0 @@
-extends Area3D
- 
-@export_multiline var dialogue: String = ""
- 
-func _on_body_entered(body):
-	if body is CharacterBody3D:
-		var dialogue_ui = get_tree().root.find_child("DialogueUI")
-		dialogue_ui.show_dialogue(dialogue)
